6bf9335da3b1c6058e08d109c20d962b746b1733
Tiago Simões <92669923+TiagoTavaresSimoes@users.noreply.github.com>
2024-03-06 16:33:25 +0000

    another tentative of fixing a bug

--- simulation.py
+++ simulation.py
@@ -13,7 +13,7 @@ class Airplane:
         self.fuel_level = max(self.fuel_level, self.emergency_fuel)
         self.fuel_level_final = self.fuel_level - self.emergency_fuel
         self.remaining_flying_time = self.fuel_level_final / self.fuel_consumption_rate
-        self.urgency = self.remaining_flying_time < 1  # urgency is true if less than one hour of fuel remains
+        self.urgency = self.remaining_flying_time < 1
 
 def generate_airplane_stream(num_airplanes, min_fuel, max_fuel, min_arrival_time, max_arrival_time):
     return [Airplane(i, min_fuel, max_fuel, min_arrival_time, max_arrival_time) for i in range(1, num_airplanes + 1)]
@@ -25,19 +25,24 @@ def schedule_landings(airplane_stream):
     landing_strip_availability = [0, 0, 0]  # Initial availability of each of the 3 landing strips
 
     for airplane in sorted_airplanes:
-        chosen_strip, next_available_time = min(enumerate(landing_strip_availability), key=lambda x: x[1])
+        #neste caso se o combustivel restante apenas der para 60 minutos de voo, o avião irá aterrar com menos de 60 minutos de combustivel (não há nada para contrariar este problema)
+        if airplane.fuel_level_final == 0:
+            airplane.fuel_level_final = airplane.fuel_level
+
+
+        chosen_strip, next_available_time_with_gap = min(
+            [(index, time + 3/60) for index, time in enumerate(landing_strip_availability)], key=lambda x: x[1])
         # Determine if this landing is urgent based on emergency fuel
         is_urgent = airplane.fuel_level_final < airplane.emergency_fuel
-
         # The actual landing time should be the later of expected landing time or next available time
-        actual_landing_time = max(airplane.expected_landing_time, next_available_time)
+        actual_landing_time = max(airplane.expected_landing_time, next_available_time_with_gap)
 
         # If the landing is urgent, the plane needs to land immediately, if possible
         if is_urgent and actual_landing_time > airplane.remaining_flying_time:
             actual_landing_time = airplane.remaining_flying_time  # Force immediate landing
 
         # Update the landing strip availability, adding 3 minutes for next landing
-        landing_strip_availability[chosen_strip] = actual_landing_time + 3 / 60  # Convert 3 minutes to hours for availability
+        landing_strip_availability[chosen_strip] = actual_landing_time + 3
 
         # Append to landing schedule
         landing_schedule.append((airplane.id, actual_landing_time, is_urgent, chosen_strip + 1))
