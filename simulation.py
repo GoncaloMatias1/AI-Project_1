077bb9e402ddf4accede7b3de43c8ec5775e9499
up202108686 <up202108686@up.pt>
2024-03-29 11:47:23 +0000

    updated landing strips usage

--- simulation.py
+++ simulation.py
@@ -25,14 +25,14 @@ def schedule_landings(airplane_stream):
     sorted_airplanes = sorted(airplane_stream, key=lambda x: x.expected_landing_time)
     landing_schedule = []
     landing_strip_availability = [0, 0, 0]  
+    landing_strip_index = 0
 
     for airplane in sorted_airplanes:
-        #neste caso se o combustivel restante apenas der para 60 minutos de voo, o avião irá aterrar com menos de 60 minutos de combustivel (não há nada para contrariar este problema)
         if airplane.fuel_level_final == 0:
             airplane.fuel_level_final = airplane.fuel_level
 
-        chosen_strip, next_available_time_with_gap = min(
-            [(index, time + 3/60) for index, time in enumerate(landing_strip_availability)], key=lambda x: x[1])
+        chosen_strip = landing_strip_index % 3
+        next_available_time_with_gap = landing_strip_availability[chosen_strip] + 3/60
         is_urgent = airplane.fuel_level_final < airplane.emergency_fuel or airplane.remaining_flying_time < airplane.expected_landing_time
         actual_landing_time = max(airplane.expected_landing_time, next_available_time_with_gap)
 
@@ -43,8 +43,9 @@ def schedule_landings(airplane_stream):
             actual_landing_time = max(actual_landing_time, airplane.expected_landing_time)
 
         landing_strip_availability[chosen_strip] = actual_landing_time + 3
-
         landing_schedule.append((airplane.id, actual_landing_time, is_urgent, chosen_strip + 1))
+
+        landing_strip_index += 1
 
     return pd.DataFrame(landing_schedule, columns=["Airplane ID", "Actual Landing Time", "Urgent", "Landing Strip"])
 
